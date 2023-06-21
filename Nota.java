19706cf1888d58ea40ab234f63183c2bb6525659
AllanFpss <101602340+AllanFpss@users.noreply.github.com>
2023-06-21 01:22:46 -0300

    Add files via upload

---
+++ Nota.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Nota {
+
+    public static void main(String args[]) {
+        double nota1=7.8;
+        double nota2=8.3;
+        double nota3=6.9;
+        double nota4=8.9;
+
+    System.out.println("********NotasSomadas********");
+
+        double notas1 = nota1 + nota2 + nota3 + nota4;
+            System.out.println(notas1);
+
+    System.out.println("********Média********");
+
+        double media = notas1 / 4;
+            System.out.println(media);
+
+    }
+}
